3ff78a5e9b75f335b4ec106983dab6e3db59a0fb
juyenajui2255-lab <juyenajui2255@gmail.com>
2026-09-14 11:44:26 +0600

    add nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,38 @@
-function app(){
-  return(
-    <h1 className="text-4xl font-bold text-blue-600"> 
-    Dev-Stack
-    </h1>
-  )
+import logo from "./assets/logo-text.png";
+
+function App() {
+  return (
+    <div>
+
+      {/* Navbar */}
+      <nav className="flex items-center justify-between px-20 py-4 border-b border-gray-400">
+
+        {/* logo */}
+        <img src={logo} alt="Dav Stack" className="w-42" />
+        {/* Menu */}
+        <div className="flex gap-6">
+          <a href="#" className="text-gray-600 hover:text-pink-400">Home</a>
+          <a href="#" className="text-gray-600 hover:text-pink-400">Technologies</a>
+          <a href="#" className="text-gray-600 hover:text-pink-400">Projects</a>
+          <a href="#" className="text-gray-600 hover:text-pink-400">About</a>
+          <a href="#" className="text-gray-600 hover:text-pink-400">Contact</a>
+        </div>
+        {/* Button */}
+        <div className="flex gap-3">
+          <button className="rounded-4xl px-5 py-2 text-black hover:bg-pink-200">
+            Sign In
+          </button>
+          <button className="rounded-4xl px-5 py-2 text-white bg-gray-800 hover:bg-black">
+            Sign Up
+          </button>
+        </div>
+      </nav>
+
+      {/* Hero Section */}
+      <section className="flex min">
+
+      </section>
+    </div>
+  );
 }
-export default app
+export default App;
